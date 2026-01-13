ac0ff59f44d572673512e69151bd75af8c6e6131
Joe <joekawa@yahoo.com>
2026-01-12 20:41:28 -0600

    completed login flow

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -55,7 +55,14 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+CORS_ALLOW_CREDENTIALS = True
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
 
 ROOT_URLCONF = 'config.urls'
 
